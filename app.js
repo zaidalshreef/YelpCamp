7aa42c881f801a52f08023a6c993640851fa05f5
zaidalshreef <zaidalshreef@gmail.com>
2019-04-23 22:03:24 +0300

    mongoosedb

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser")
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://zaid:987654321@yelpcamp-ekagq.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+
+const campschema = mongoose.Schema({
+    name: String,
+    image: String,
+});
+const camp = mongoose.model('camp', campschema);
 
 
-let campground = [
-    { name: "nice", image: "https://res.cloudinary.com/yelpcampcloud/image/upload/v1511259835/fobqrkzouhko776owgj4.jpg" },
-    { name: "amazing campground", image: "https://threerivers-drupal.s3.us-east-2.amazonaws.com/public/2017-03/CL_camping_Billboard_01.jpg" },
-    { name: "nice", image: "https://res.cloudinary.com/yelpcampcloud/image/upload/v1511259835/fobqrkzouhko776owgj4.jpg" },
-    { name: "amazing", image: "https://threerivers-drupal.s3.us-east-2.amazonaws.com/public/2017-03/CL_camping_Billboard_01.jpg" },
-    { name: "trees green", image: "https://www.appletonmn.com/vertical/Sites/%7B4405B7C1-A469-4999-9BC5-EC3962355392%7D/uploads/campground_(2).jpg" },
-    { name: "nice trees", image: "https://res.cloudinary.com/yelpcampcloud/image/upload/v1511259835/fobqrkzouhko776owgj4.jpg" },
-    { name: "trees", image: "https://www.appletonmn.com/vertical/Sites/%7B4405B7C1-A469-4999-9BC5-EC3962355392%7D/uploads/campground_(2).jpg" },
-    { name: "nice campgrounds", image: "https://res.cloudinary.com/yelpcampcloud/image/upload/v1511259835/fobqrkzouhko776owgj4.jpg" },
-
-];
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
-    res.render("landing");
 
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+app.get('/campgrounds', (req, res) => {
+
+    camp.find({}, (err, allcamp) => {
+        if (err) {
+            console.log("error");
+        } else {
+            res.render('campgrounds', { campgrounds: allcamp });
+
+        }
+    });
+});
+app.post('/campgrounds', (req, res) => {
+    camp.create(req.body);
+    res.redirect('/campgrounds');
+});
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new');
 });
 
-app.get("/campgrounds", function(req, res) {
 
-    res.render("campgrounds", { campgrounds: campground })
-});
-app.post("/campgrounds", function(req, res) {
-    let campg = req.body;
-    campground.push(campg);
-    res.redirect("/campgrounds");
-});
-app.get("/campgrounds/new", function(req, res) {
-
-    res.render("new");
-});
-
-let port = process.env.port || 3000;
-
-app.listen(port, process.env.IP, function() {
-    console.log(port);
+app.listen(3000, () => {
+    console.log(3000);
 });
